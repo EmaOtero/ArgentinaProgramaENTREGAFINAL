c4f3cc5d4268229f67cfdb6f106afa8bf83103a0
Criss0001 <cristianjfarina@gmail.com>
2023-04-17 18:20:27 -0300

    Cambios Formulario

---
+++ js/formulario_jquery.js
@@ -0,0 +1,45 @@
+
+
+$.validator.addMethod("terminarPor", function(value, element, parametro){
+
+
+    if(value.endsWith(parametro)){
+        return true;
+    }
+
+    return false;
+
+}, "Debe terminar por {0}")
+
+
+
+
+$("#formulario_contacto").validate({
+    rules: {
+        nombre: {
+            required: true,
+            minlength: 3,
+            maxlenght: 30
+        },
+        correo: {
+            required: true,
+            email: true,
+            terminaPor; "duoc.cl"
+        },
+
+
+
+    }
+})
+
+
+$("#guardar").click(function) {
+    if($("#formulario_contacto").valid() == false) {
+        return;
+    }
+    let nombre = $("#nombre").val()
+    let apellido = $("#apellido").val()
+    let correo = $("#correo").val()
+    let telefono = $("#telefono").val()
+    
+}
